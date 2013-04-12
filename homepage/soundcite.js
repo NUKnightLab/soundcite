ee18ec814230b83c6090eda1d92769cdc9ebdf8f
Tyler Fisher <bassist201@gmail.com>
2013-04-12 16:47:53 -0500

    actually removed logging and other, unnecessary commented out code. a little bit of explanation in the code too

--- homepage/soundcite.js
+++ homepage/soundcite.js
@@ -1,26 +1,32 @@
 $(document).ready(function () {
+    // global vars
     var start;
     var end;
+    var ids = [];
 
     // borrowing underscore.js bind function
-
     var scBind = function(func, context) {
-        if (func.bind === nativeBind && nativeBind) return nativeBind.apply(func, slice.call(arguments, 1));
         var args = slice.call(arguments, 2);
         return function() {
-          return func.apply(context, args.concat(slice.call(arguments)));
+            return func.apply(context, args.concat(slice.call(arguments)));
         };
     };
 
+    // initialize SoundCloud SDK
     SC.initialize({
         client_id: "5ba7fd66044a60db41a97cb9d924996a",
     });
+
+    // load player chrome
     $('body').append('<link rel="stylesheet" href="http://soundcite.com/player.css" type="text/css" />');
+
+    // get all soundcite objects and grab their SoundCloud ids
     var soundcite_array = $('.soundcite');
-    var ids = [];
     for (i = 0; i < soundcite_array.length; i++) {
         ids.push(soundcite_array[i].attributes['data-id'].value);
     }
+
+    //load each sound and attach click handler
     for (i = 0; i < ids.length; i++) {
         SC.stream(ids[i], function(sound) {
             sound.load({
@@ -33,25 +39,15 @@ $(document).ready(function () {
                 start = clicked.attr('data-start');
                 end = clicked.attr('data-end');
                 var this_id = clicked.attr('data-id');
-                // console.log(sound.url);
-                // console.log(start)
-                // console.log(end)
                 sound.setPosition(start);
-                // sound.onPosition(end, function() {
-                //     sound.pause();
-                // });
                 if (sound.url == 'http://api.soundcloud.com/tracks/' + this_id + '/stream?client_id=5ba7fd66044a60db41a97cb9d924996a' ) {
                     sound.play({
                         whileplaying: function() {
-                            // console.log('working');
                             var totalTime = end - start;
                             var position = sound.position;
                             var relative_position = position - start;
                             var percentage = (relative_position / totalTime) * 100
                             clicked.css({'background' : '-webkit-linear-gradient(left, white, #ccc ' + percentage + '%, white)'});
-                            // clicked.click(function() {
-                            //     sound.pause();
-                            // })
 
                             if (sound.position > end) {
                                 sound.pause();
