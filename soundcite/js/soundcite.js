2ce333d7982f0668caf988c83e56149d3c8b27d9
Joe Germuska <joe@germuska.com>
2013-07-03 17:10:48 -0500

    remove analytics from master branch until we make sure they work

--- soundcite/js/soundcite.js
+++ soundcite/js/soundcite.js
@@ -21,37 +21,17 @@
                     'background' : '-webkit-linear-gradient(left, rgba(0,0,0,.15)' + percentage + '%, rgba(0,0,0,.05)' + (percentage + 1) + '%)',
                     'background' : 'linear-gradient(to right, rgba(0,0,0,.15)' + percentage + '%, rgba(0,0,0,.05)' + (percentage + 1) + '%)'
                 });
-            },
-            // Use google analytics, set to false for no tracking
-            use_analytics: true
-        };
-        $.extend(SOUNDCITE_CONFIG, window.SOUNDCITE_CONFIG)
-  
-        // add google analytics
-        var _gaq = _gaq || [];
-        if(SOUNDCITE_CONFIG.use_analytics) {            
-            _gaq.push(
-                function() {
-                    var pageTracker = _gat._createTracker('UA-34830490-1', 'soundciteTracker');
-                 },
-                ['soundciteTracker._setDomainName', 'knightlab.com']
-                ['soundciteTracker._setAllowLinker', true], 
-                ['soundciteTracker._trackPageview']
-            );
-            (function() {
-                var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-                ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-                var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-            })();
-        }
+            }
 
+        }
+        $.extend(SOUNDCITE_CONFIG, window.SOUNDCITE_CONFIG)
         // global vars
         window.soundcite = {};
 
         var start;
         var end;
         var clips = [];
-        
+
         // initialize SoundCloud SDK
         SC.initialize({
             client_id: "5ba7fd66044a60db41a97cb9d924996a",
@@ -137,11 +117,6 @@
             });
             this.playing = true;
             this.times_played++;
-            
-            // track play event
-            if(SOUNDCITE_CONFIG.use_analytics) {
-                _gaq.push(['soundciteTracker._trackEvent', 'soundcite', 'play', 'soundcloud:'+this.id]); 
-            }
         }
 
         Clip.prototype.pause_clip = function() {
@@ -149,10 +124,6 @@
             this.$el.addClass('soundcite-play');
             this.playing = false;
             this.sound.pause();
-            // track play event
-            if(SOUNDCITE_CONFIG.use_analytics) {
-                _gaq.push(['soundciteTracker._trackEvent', 'soundcite', 'pause', 'soundcloud:'+this.id]); 
-            }
         }
 
         Clip.prototype.track_progress = function() {
