95a9dec104ccbed85099da7366fe2d6379013ab4
jywsn <jennifer.y.wilson@northwestern.edu>
2014-03-17 14:47:03 -0500

    Transition to fablib;do not require secrets for build or serve

--- website/core/settings/loc.py
+++ website/core/settings/loc.py
@@ -3,12 +3,11 @@ import sys
 from os.path import normpath, join
 from .base import *
 
-# Import secrets
-sys.path.append(
-    abspath(join(PROJECT_ROOT, '../secrets/buzz/stg'))
-)
-
-from secrets import *
+# Import secrets -- not needed for this project
+#sys.path.append(
+#    abspath(join(PROJECT_ROOT, '../secrets/soundcite/stg'))
+#)
+#from secrets import *
 
 # Set static URL
 STATIC_URL = '/static'
