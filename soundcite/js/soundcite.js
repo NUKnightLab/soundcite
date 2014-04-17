c3b38c4395c2f5211034c3bed00cfc9206e2e237
jywsn <jennifer.y.wilson@northwestern.edu>
2014-04-17 15:34:39 -0500

    add example mp3 playback using popcornjs

--- soundcite/js/soundcite.js
+++ soundcite/js/soundcite.js
@@ -1,21 +1,52 @@
+// window.Popcorn.version = 1.5.6
+// http://popcornjs.org/code/dist/popcorn-complete.min.js
+
 (function(window, document, version, callback) { // http://stackoverflow.com/questions/2170439/how-to-embed-javascript-widget-that-depends-on-jquery-into-an-unknown-environmen
-    var j, d;
-    var loaded = false;
-    if (!(j = window.jQuery) || version > j.fn.jquery || callback(j, loaded)) {
-        var script = document.createElement("script");
-        script.type = "text/javascript";
-        script.src = "http://code.jquery.com/jquery-1.9.1.min.js";
-        script.onload = script.onreadystatechange = function() {
-            if (!loaded && (!(d = this.readyState) || d == "loaded" || d == "complete")) {
-                callback((j = window.jQuery).noConflict(1), loaded = true);
-                j(script).remove();
-            }
-        };
-        // document.head not standard before HTML5
-        var insertionPoint = document.head || document.getElementsByTagName('head').item(0) || document.documentElement.childNodes[0];
-        insertionPoint.appendChild(script)
+    var loaded_j = false;
+    var loaded_p = false;   
+    // document.head not standard before HTML5
+    var insertionPoint = document.head || document.getElementsByTagName('head').item(0) || document.documentElement.childNodes[0];
+    
+    function load_jquery(version, cb) {
+        var js, d;       
+        if (!(js = window.jQuery) || version > js.fn.jquery || cb(js)) {
+            var script = document.createElement("script");
+            script.type = "text/javascript";
+            script.src = "http://code.jquery.com/jquery-1.9.1.min.js";       
+            script.onload = script.onreadystatechange = function() {
+                if(!loaded_j && (!(d = this.readyState) || d == "loaded" || d == "complete")) {
+                    js = window.jQuery.noConflict(1);
+                    cb(js, loaded_j = true);
+                    js(script).remove();               
+                }
+            };       
+            insertionPoint.appendChild(script);
+        } 
     }
-})(window, document, "1.3", function($, jquery_loaded) {
+        
+    function load_popcorn(j, version, cb) {
+        var js, d;      
+        if(!(js = window.Popcorn) || version > js.version || cb(js)) {
+            var script = document.createElement("script");
+            script.type = "text/javascript";
+            script.src = "http://popcornjs.org/code/dist/popcorn-complete.min.js";
+            script.onload = script.onreadystatechange = function() {
+                if(!loaded_p && (!(d = this.readyState) || d == "loaded" || d == "complete")) {
+                    cb(window.Popcorn, loaded_p = true);
+                    j(script).remove();               
+                }
+            };       
+            insertionPoint.appendChild(script);
+        } 
+    }
+    
+    load_jquery(version, function(j) {
+        load_popcorn(j, "1.5.6", function(p) {
+            callback(j, p);       
+        });
+    });
+     
+})(window, document, "1.3", function($, $Popcorn) {
     $(document).ready(function () {
         var SOUNDCITE_CONFIG = {
             update_playing_element: function(el, percentage) {
@@ -24,15 +55,15 @@
                     'background' : 'linear-gradient(to right, rgba(0,0,0,.15)' + percentage + '%, rgba(0,0,0,.05)' + (percentage + 1) + '%)'
                 });
             }
-
         }
         $.extend(SOUNDCITE_CONFIG, window.SOUNDCITE_CONFIG)
         // global vars
         window.soundcite = {};
 
-        var start;
-        var end;
         var clips = [];
+        var $audio = $('<div class="soundcite-audio"></div>');                  // TODO: style to hide
+        
+        $('body').append($audio);
 
         // initialize SoundCloud SDK
         SC.initialize({
@@ -48,50 +79,94 @@
             };
         };
 
-        // create clip constructor
+// ------------------------------------------------------------
+// Generic Clip
+
         function Clip(el) {
             this.el = el;
             this.$el = $(this.el);
-            this.id = el.attributes['data-id'].value;
-            this.start = el.attributes['data-start'].value;
+            this.start = el.attributes['data-start'].value || 0;
             this.end = el.attributes['data-end'].value;
             this.playing = false;
-            this.times_played = 0;
-            SC.stream(this.id,bind(function(sound) {
-                this.sound = sound;
-                sound.load({
-                    onload: bind(this.sound_loaded, this)
-                });
-            }, this));
-        };
-
-        //clip methods
+            this.times_played = 0;            
+            this.sound = null;                          // implement in subclass
+        }
+        
         Clip.prototype.sound_loaded = function() {
             this.$el.click(bind(this.click_handler, this));
-            this.$el.addClass('soundcite-loaded soundcite-play');
+            this.$el.addClass('soundcite-loaded soundcite-play');        
+        }
+        
+        Clip.prototype.play = function() {
+            this.$el.removeClass('soundcite-play');
+            this.$el.addClass('soundcite-pause');           
+            this.play_sound();                          // implement in subclass
+            this.playing = true;
+            this.times_played++;
+        }
+
+        Clip.prototype.pause = function() {
+            this.$el.removeClass('soundcite-pause');
+            this.$el.addClass('soundcite-play');
+            this.playing = false;
+            this.pause_sound();                         // implement in subclass
+        }
+        Clip.prototype.stop = function() {    
+            console.log('stop');
+            this.stop_sound();                          // implement in subclass
+            this.playing = false;
+            this.position_sound(this.start);            // implement in subclass
+            this.$el.removeClass('soundcite-pause');
+            this.$el.addClass('soundcite-play');            
+        }
+
+        Clip.prototype.track_progress = function() {
+            var totalTime = this.end - this.start;
+            var position = this.sound_position();       // implement in subclass
+            var relative_position = position - this.start;
+            var percentage = (relative_position * 100) / totalTime;            
+            SOUNDCITE_CONFIG.update_playing_element(this.el, percentage);
         }
 
         Clip.prototype.click_handler = function() {
-            //check for other playing clips and stop them
-            for (i=0; i<clips.length; i++) {
-                if (this.el !== clips[i].el) {
-                    clips[i].sound.stop();
-                    clips[i].playing = false;
-                    clips[i].sound.setPosition(clips[i].start);
-                    $(clips[i].el).removeClass('soundcite-pause');
-                    $(clips[i].el).addClass('soundcite-play');
-                }
+            // stop other playing clips
+            for(var i = 0; i < clips.length; i++) {
+                if(this.el !== clips[i].el) {
+                    clips[i].pause();
+                 }
             }
 
-            if (!this.playing) {
-                this.play_clip();
-            }
-            else {
-                this.pause_clip();
+            if(!this.playing) {
+                this.play();
+            } else {
+                this.pause();
             }
         }
 
-        Clip.prototype.play_clip = function() {
+// ------------------------------------------------------------
+// SoundCloud Clip
+
+        function SoundCloudClip(el) {
+            Clip.apply(this, Array.prototype.slice.call(arguments));
+
+            this.id = el.attributes['data-id'].value;
+
+            SC.stream(this.id, bind(function(sound) {
+                this.sound = sound;
+                sound.load({onload: bind(this.sound_loaded, this)});
+            }, this));
+        }
+        SoundCloudClip.prototype = Object.create(Clip.prototype);
+
+        SoundCloudClip.prototype.sound_position = function() {
+            return this.sound.position;
+        }
+
+        SoundCloudClip.prototype.position_sound = function(pos) {
+            this.sound.setPosition(pos);
+        }
+        
+        SoundCloudClip.prototype.play_sound = function() {
             if (this.times_played == 0 || this.sound.position > this.end) {
                 this.sound.setPosition(this.start);
             }
@@ -99,48 +174,94 @@
                 this.sound.setPosition(this.sound.position);
             }
 
-            this.$el.removeClass('soundcite-play');
-            this.$el.addClass('soundcite-pause');
-
             this.sound.play({
                 whileplaying: bind(function() {
                     this.track_progress();
 
-                    if (this.sound.position > this.end) {
-                        this.$el.removeClass('soundcite-pause');
-                        this.$el.addClass('soundcite-play');
-                        // this.$el.css({
-                        //     'background': 'rgba(0,0,0,.05)'
-                        // })
-                        this.sound.stop();
-                        this.playing = false;
+                    if(this.sound.position > this.end) {
+                        this.stop();
                     }
                 }, this),
             });
-            this.playing = true;
-            this.times_played++;
         }
-
-        Clip.prototype.pause_clip = function() {
-            this.$el.removeClass('soundcite-pause');
-            this.$el.addClass('soundcite-play');
-            this.playing = false;
+        
+        SoundCloudClip.prototype.pause_sound = function() { 
             this.sound.pause();
         }
-
-        Clip.prototype.track_progress = function() {
-            var totalTime = this.end - this.start;
-            var position = this.sound.position;
-            var relative_position = position - this.start;
-            var percentage = (relative_position / totalTime) * 100
-            SOUNDCITE_CONFIG.update_playing_element(this.el, percentage);
+        
+        SoundCloudClip.prototype.stop_sound = function() {
+            this.sound.stop();
+        }
+        
+// ------------------------------------------------------------
+// Popcorn Clip
+    
+        function PopcornClip(el) {
+            Clip.apply(this, Array.prototype.slice.call(arguments));
+                     
+            this.url = el.attributes['data-url'].value;
+            
+            var id = 'soundcite-audio-'+clips.length;
+            $audio.append('<audio id="'+id+'">'
+                        + '<source src="'+this.url+'" />'
+                        + '</audio>');   
+                        
+            this.sound = $Popcorn('#'+id, {'frameAnimation': true});
+            
+            this.sound.on('loadeddata', bind(function() {
+                if(!this.end) {
+                    this.end = this.sound.duration();
+                }   
+                  
+                this.sound.cue(this.end, bind(this.stop, this)); 
+                this.sound_loaded();                
+            }, this));
+                        
+        } 
+        PopcornClip.prototype = Object.create(Clip.prototype);
+     
+        PopcornClip.prototype.sound_position = function() {
+            return this.sound.currentTime();
         }
 
-        // set up clips array
+        PopcornClip.prototype.position_sound = function(pos) {
+            this.sound.currentTime(pos);
+        }
+
+        PopcornClip.prototype.play_sound = function() {            
+            if (this.times_played == 0 || this.sound.roundTime() > this.end) {
+                this.sound.play(this.start)
+            } else {   
+                this.sound.play();
+            }       
+            
+            this.sound.on('timeupdate', bind(this.track_progress, this));
+            this.sound.on('ended', bind(this.stop, this));
+        }
+        
+        PopcornClip.prototype.pause_sound = function() {
+            this.sound.pause();      
+            this.sound.off('timeupdate');
+        }
+        
+        PopcornClip.prototype.stop_sound = function() {
+            this.sound.pause();
+            this.sound.off('timeupdate');
+        }
+
+// ------------------------------------------------------------
+// set up clips array
+
         var soundcite_array = $('.soundcite');
-        for (i = 0; i < soundcite_array.length; i++) {
-            clips.push(new Clip(soundcite_array[i]));
+        for(var i = 0; i < soundcite_array.length; i++) {
+            var el = soundcite_array[i];          
+            if(el.hasAttribute('data-url')) {
+                clips.push(new PopcornClip(el));
+            } else {
+                clips.push(new SoundCloudClip(el));
+            }
         }
         soundcite.Clip = Clip;
     });
+    
 });
