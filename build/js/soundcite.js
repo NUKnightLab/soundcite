473d9b91b1c83c77d4a6b6410f2b1a5a06452dc6
Joe Germuska <joe@germuska.com>
2016-03-16 14:38:24 -0500

    use HTTPS everywhere instead of relative URLs. Friendlier to file:/// urls for local dev

--- build/js/soundcite.js
+++ build/js/soundcite.js
@@ -1,5 +1,5 @@
-/* soundcite - v0.4.1 - 2015-05-11
- * Copyright (c) 2015 Tyler J. Fisher and Northwestern University Knight Lab 
+/* soundcite - v2016-03-15-13-07-50 - 2016-03-15
+ * Copyright (c) 2016 Tyler J. Fisher and Northwestern University Knight Lab 
  */
 //loop branch
 // window.Popcorn.version = 1.5.6
@@ -10,13 +10,13 @@
 (function(window, document, callback) { // http://stackoverflow.com/questions/2170439/how-to-embed-javascript-widget-that-depends-on-jquery-into-an-unknown-environmen
     var loaded_p = true;    // default is to not load popcornjs
     var loaded_s = true;    // default is to not load soundcloud
-    
+
     // document.head not standard before HTML5
     var insertionPoint = document.head || document.getElementsByTagName('head').item(0) || document.documentElement.childNodes[0];
 
     function cmp_vers(v1, v2) {
         var a1 = v1.split('.');
-        var a2 = v2.split('.');       
+        var a2 = v2.split('.');
         for(var i = 0; i < Math.min(a1.length, a2.length); i++) {
             var n1 = parseInt(a1[i]);
             var n2 = parseInt(a2[i]);
@@ -24,19 +24,19 @@
             if(d) {
                 return d;
             }
-        }    
+        }
         return(a1.length - a2.length);
     }
 
     function load_popcorn(version, cb) {
-        var js = window.Popcorn, d, new_js;     
-        
+        var js = window.Popcorn, d, new_js;
+
         if(loaded_p) {
             cb(js);
         } else if(!js || cmp_vers(version, js.version) > 0 || cb(js)) {
             var script = document.createElement("script");
             script.type = "text/javascript";
-            script.src = "//cdn.knightlab.com/libs/soundcite/latest/vendor/popcorn.min.js"; // no SSL version of this, will self-host
+            script.src = "https://cdn.knightlab.com/libs/soundcite/latest/vendor/popcorn.min.js"; // no SSL version of this, will self-host
             script.onload = script.onreadystatechange = function() {
                 if(!loaded_p && (!(d = this.readyState) || d == "loaded" || d == "complete")) {
                     new_js = window.Popcorn;
@@ -44,17 +44,17 @@
                         window.Popcorn = js;
                     }
                     cb(new_js, loaded_p = true);
-                    insertionPoint.removeChild(script);           
+                    insertionPoint.removeChild(script);
                 }
-            };       
+            };
             insertionPoint.appendChild(script);
-        } 
+        }
     }
 
     // Loading player api initializes incomplete version of window.SC
     function load_soundcloud(version, cb) {
         var js = window.SC, d;
-        
+
         if(loaded_s) {
             cb(js);
         } else if(!js || !js.Dialog || cmp_vers(version, js._version) > 0 || cb(js)) {
@@ -67,10 +67,10 @@
                     insertionPoint.removeChild(script);
                 }
             };
-            insertionPoint.appendChild(script);        
-        }    
+            insertionPoint.appendChild(script);
+        }
     }
-  
+
     // http://dustindiaz.com/smallest-domready-ever
     function r(f) {
         if(/in/.test(document.readyState)) {
@@ -78,27 +78,27 @@
         } else {
             f();
         }
-    } 
-    
+    }
+
     r(function() {
         var elements = document.getElementsByClassName('soundcite');
-        
+
         for(var i = 0; i < elements.length; i++) {
             if(elements[i].getAttribute('data-url')) {
                 loaded_p = false;   // needs popcorn
             } else {
                 loaded_s = false;   // needs soundcloud
-            }        
+            }
         }
-     
+
         load_popcorn("1.5.6", function(p) {
             load_soundcloud("2.0.0", function(s) {
-               callback(elements, p, s);   
-            });                
+               callback(elements, p, s);
+            });
         });
     });
-    
-})(window, document, function(soundcite_elements, $Popcorn, $SoundCloud) {    
+
+})(window, document, function(soundcite_elements, $Popcorn, $SoundCloud) {
 
     // borrowing underscore.js bind function
     var bind = function(func, context) {
@@ -108,15 +108,15 @@
           return func.apply(context, args.concat(slice.call(arguments)));
         };
     };
-       
+
     function removeClass(el, name) {
-        var cn = el.className;        
+        var cn = el.className;
         for(var i = 0, arr = name.match(/\S+/g); i < arr.length; i++) {
             cn = cn.replace(new RegExp('(?:^|\\s)'+arr[i]+'(?!\\S)'), '');
         }
-        el.className = cn; 
+        el.className = cn;
     }
-    
+
     function addClass(el, name) {
         var cn = el.className;
         for(var i = 0, arr = name.match(/\S+/g); i < arr.length; i++) {
@@ -126,22 +126,22 @@
         }
         el.className = cn;
     }
-    
+
     var SOUNDCITE_CONFIG = {
-        update_playing_element: function(el, percentage) {        
-             el.style.cssText = 
+        update_playing_element: function(el, percentage) {
+             el.style.cssText =
                 'background: -webkit-linear-gradient(left, rgba(0,0,0,.15)' + percentage + '%, rgba(0,0,0,.05)' + (percentage + 1) + '%);'
               + 'background: linear-gradient(to right, rgba(0,0,0,.15)' + percentage + '%, rgba(0,0,0,.05)' + (percentage + 1) + '%);'
         }
-    }        
+    }
     for(var key in window.SOUNDCITE_CONFIG) {
         SOUNDCITE_CONFIG[key] = window.SOUNDCITE_CONFIG[key];
     }
-    
+
     // global vars
     window.soundcite = {};
 
-    // check for mobile        
+    // check for mobile
     if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
         soundcite.mobile = true;
     } else {
@@ -149,7 +149,7 @@
     }
 
     var clips = [];
-    
+
     // setup audio container
     var audio_container = document.createElement('div');
     addClass(audio_container, 'soundcite-audio');
@@ -163,7 +163,7 @@
     }
 
     // pause all playing clips. Optionally pass a clip as an argument which
-    // should not be paused, for the case that this is triggered from a 
+    // should not be paused, for the case that this is triggered from a
     // click handler.
     function pause_all_clips(except_clip){
         for(var i = 0; i < clips.length; i++) {
@@ -178,32 +178,32 @@
 // Clip
     function Clip(el) {
         this.el = el;
-        this.start = el.hasAttribute('data-start') ? el.getAttribute('data-start') : 0; // ms          
+        this.start = el.hasAttribute('data-start') ? el.getAttribute('data-start') : 0; // ms
         this.end = el.hasAttribute('data-end') ? el.getAttribute('data-end') : null;    // ms
-        
+
         this.plays = el.hasAttribute('data-plays') ? parseInt(el.getAttribute('data-plays')) : 1;
         this.plays_left = this.plays;
-        
+
         this.playing = false;
-        this.sound = null;                          // implement in subclass                  
+        this.sound = null;                          // implement in subclass
         clips.push(this);   // keep track of this
     }
-    
+
     Clip.prototype.sound_loaded = function() {
         this.el.addEventListener('click', bind(this.click_handler, this));
         addClass(this.el, 'soundcite-loaded soundcite-play');
     }
-    
+
     Clip.prototype.pause = function() {
         removeClass(this.el, 'soundcite-pause');
         addClass(this.el, 'soundcite-play');
         this.pause_sound();                         // implement in subclass
         this.playing = false;
     }
-    
-    Clip.prototype.stop = function() {    
+
+    Clip.prototype.stop = function() {
         removeClass(this.el, 'soundcite-pause');
-        addClass(this.el, 'soundcite-play');            
+        addClass(this.el, 'soundcite-play');
         this.stop_sound();                          // implement in subclass
         this.playing = false;
         this.plays_left = this.plays;    // reset plays_left!
@@ -213,7 +213,7 @@
         var totalTime = this.end - this.start;
         var position = this.sound_position();       // implement in subclass
         var relative_position = position - this.start;
-        var percentage = (relative_position * 100) / totalTime;            
+        var percentage = (relative_position * 100) / totalTime;
         SOUNDCITE_CONFIG.update_playing_element(this.el, percentage);
     }
 
@@ -232,16 +232,16 @@
         Clip.apply(this, Array.prototype.slice.call(arguments));
 
         this.id = el.getAttribute('data-id');
-        
+
         $SoundCloud.stream(this.id, bind(function(sound) {
             this.sound = sound;
 
             this.sound._player.on("positionChange", bind(function(pos) {
-                this.track_progress(); 
-            
-                if(pos >= this.end) {              
+                this.track_progress();
+
+                if(pos >= this.end) {
                     if(this.plays) {
-                        this.plays_left--;  // update plays_left                        
+                        this.plays_left--;  // update plays_left
                         if(this.plays_left > 0) {
                             this.play();
                             this.track_position();
@@ -252,13 +252,13 @@
                         this.play();
                         this.track_position();
                     }
-                }  
+                }
             }, this));
-            
+
             if(this.end === null) {
                 this.end = this.sound.getDuration();
             }
-           
+
            this.sound_loaded();
         }, this));
     }
@@ -267,11 +267,11 @@
     SoundCloudClip.prototype.sound_position = function() {
         return this.sound.getCurrentPosition();
     }
-                    
-    SoundCloudClip.prototype.pause_sound = function() { 
+
+    SoundCloudClip.prototype.pause_sound = function() {
         this.sound.pause();
     }
-    
+
     SoundCloudClip.prototype.stop_sound = function() {
         this.sound.stop();
     }
@@ -282,23 +282,23 @@
         if(pos < this.start || pos >= this.end) {
             this.sound.seek(this.start);
         }
-        
+
         removeClass(this.el, 'soundcite-play');
         addClass(this.el, 'soundcite-pause');
 
-        this.sound.play();                        
+        this.sound.play();
         this.playing = true;
     }
-            
-// Popcorn Clip    
+
+// Popcorn Clip
     function PopcornClip(el) {
         Clip.apply(this, Array.prototype.slice.call(arguments));
 
         this.id = 'soundcite-audio-'+clips.length;
         this.url = el.getAttribute('data-url');
-      
+
         // convert to ms to secs
-        this.start = Math.floor(this.start / 1000);       
+        this.start = Math.floor(this.start / 1000);
         if(this.end !== null) {
             this.end = Math.floor(this.end / 1000);
         }
@@ -307,17 +307,17 @@
         audio.id = this.id;
         audio.setAttribute("src", this.url);
         audio.setAttribute("preload", "true");
-        
-        audio_container.appendChild(audio);            
-                           
+
+        audio_container.appendChild(audio);
+
         this.sound = $Popcorn('#'+this.id, {'frameAnimation': true});
-                    
-        // Safari iOS Audio streams cannot be loaded unless triggered by a 
+
+        // Safari iOS Audio streams cannot be loaded unless triggered by a
         // user event, so load in play_sound via click for mobile
         this.sound.on('loadeddata', bind(function() {
             if(this.end === null) {
                 this.end = this.sound.duration();
-            }                  
+            }
 
             this.sound.cue(this.end, bind(function() {
                 if(this.plays) {
@@ -331,39 +331,39 @@
                     }
                 } else {                    // infinite loop
                     this.sound.currentTime(this.start);
-                    this.play();               
+                    this.play();
                 }
             }, this));
-            
+
             if(!soundcite.mobile) {
                 this.sound_loaded();
             }
         }, this));
 
-        if(soundcite.mobile) {   
+        if(soundcite.mobile) {
             this.sound_loaded();
         } else if(this.sound.readyState() > 1) {
             this.sound_loaded();
         }
-    } 
+    }
     PopcornClip.prototype = Object.create(Clip.prototype);
- 
+
     PopcornClip.prototype.sound_position = function() {
         return this.sound.currentTime();
     }
 
     PopcornClip.prototype.pause_sound = function() {
-        this.sound.pause();      
+        this.sound.pause();
         this.sound.off('timeupdate');
     }
-    
+
     PopcornClip.prototype.stop_sound = function() {
         this.sound.pause();
         this.sound.off('timeupdate');
     }
 
     PopcornClip.prototype._play_sound = function() {
-        removeClass(this.el, 'soundcite-loading soundcite-play');       
+        removeClass(this.el, 'soundcite-loading soundcite-play');
         addClass(this.el, 'soundcite-pause');
 
         this.sound.play();
@@ -371,38 +371,38 @@
 
         this.sound.on('timeupdate', bind(this.track_progress, this));
     }
-    
+
     PopcornClip.prototype.play_sound = function() {
         var pos = this.sound.roundTime();
-        
+
         if(pos < this.start || pos >= this.end) {
             this.sound.on('seeked', bind(function() {
                 this.sound.off('seeked');
-                this._play_sound();                
+                this._play_sound();
             }, this));
-            
+
             this.sound.currentTime(this.start);
         } else {
-            this._play_sound();                         
+            this._play_sound();
         }
     }
-         
-    PopcornClip.prototype.play = function() {           
-        if(soundcite.mobile) { 
+
+    PopcornClip.prototype.play = function() {
+        if(soundcite.mobile) {
             removeClass(this.el, 'soundcite-play');
             addClass(this.el, 'soundcite-loading');
-                 
+
             if(this.sound.readyState() > 1) {
                 this.play_sound();
-            } else {               
+            } else {
                 this.sound.on('canplaythrough', bind(function() {
                     this.play_sound();
                 }, this));
                 document.getElementById(this.id).load();
-            }  
+            }
         } else {
             this.play_sound();
-        }       
+        }
     }
 
 // set up clips array from element array
@@ -411,11 +411,11 @@
         if(el.getAttribute('data-url')) {
             new PopcornClip(el);
         } else {
-            new SoundCloudClip(el);        
-        }        
+            new SoundCloudClip(el);
+        }
     }
-    
-    
+
+
     soundcite.Clip = Clip;
     soundcite.SoundCloudClip = SoundCloudClip;
     soundcite.PopcornClip = PopcornClip;
